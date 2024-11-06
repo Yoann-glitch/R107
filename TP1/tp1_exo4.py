1dfdf4dd3afbb2ffb1ff529aabd87447742585aa
Yoann-glitch <yoyo.mul007@gmail.com>
2024-11-06 11:01:01 +0100

    test sup change

---
+++ TP1/tp1_exo4.py
@@ -0,0 +1,6 @@
+nombre = input()
+nombre = int (nombre)
+
+jour = nombre /24/60
+
+print ("Il s'est passé en tout {:.0f} jour de ce mois".format(jour))
--- tp1_exo4.py
+++
@@ -1,6 +0,0 @@
-nombre = input()
-nombre = int (nombre)
-
-jour = nombre /24/60
-
-print ("Il s'est passé en tout {:.0f} jour de ce mois".format(jour))
