3c050f2c42de388a39f881c0d4e086448d062392
Yoann-glitch <yoyo.mul007@gmail.com>
2024-11-06 10:33:10 +0100

    init

---
+++ tp1_exo3.py
@@ -0,0 +1,11 @@
+jour= input()
+heure= input()
+minute= input()
+jour = int (jour)
+heure = int (heure)
+minute=int (minute)
+
+total = (jour*24*60)+(heure*60)+(minute)
+
+print (f"Il s'est passé{total} minutes depuis le début du mois");
+
