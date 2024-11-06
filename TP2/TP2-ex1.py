0fbe7d52d592e76e0001f820fdbf63e4f5fcd2c9
Yoann-glitch <yoyo.mul007@gmail.com>
2024-11-06 11:40:22 +0100

    test sup change

---
+++ TP2/TP2-ex1.py
@@ -0,0 +1,19 @@
+x = input("Entrez x" )
+y = input("Entrez y")
+z = 0
+
+x= int(x)
+y= int(y)
+
+
+print("Avant permutation")
+print("x:",x)
+print("y:",y)
+z = x
+x = y
+y = z
+
+
+print("Après permutation")
+print("x:",x)
+print("y:",y)
