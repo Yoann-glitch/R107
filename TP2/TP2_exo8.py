e1e389358ae37fb6eebcb4fdc73afffeed38ae83
Yoann-glitch <yoyo.mul007@gmail.com>
2024-11-08 14:25:04 +0100

    TP2

--- TP2/TP2_exo8.py
+++ TP2/TP2_exo8.py
@@ -3,4 +3,4 @@ while True:
     if (x >= 2 and x < 3) or (x > 0 and x <= 1) or (x >= -10 and x <= -2):
         print("x est inclu dans I\n")
     else:
-        print("x est exclu de  I\n")
+        print("x n'est pas inclu dans I\n")
