13987d983da997b4d2f8ec8a71d65e572e939426
install <yoyo.mul007@gmail.com>
2024-11-22 14:15:13 +0100

    TP4

--- TP4/TP4_exo1.py
+++ TP4/TP4_exo1.py
@@ -4,7 +4,7 @@ table_multiplication = []
 
 for i in range(10):
     resultat = nombre * i
-    table_multiplication.append(round(resultat, 1))
+    table_multiplication.append(round(resultat, 1))#round permet de réduire le nombre de chiffre après la virgule
 
 for i in range(10):
     print(f"{nombre} * {i} = {table_multiplication[i]}")
