3aa93c9c139882d2fbf27d490ba7398cd7b60516
install <yoyo.mul007@gmail.com>
2024-11-28 08:57:40 +0100

    TP4

---
+++ TP4/TP4_exo7.py
@@ -0,0 +1,6 @@
+binome = ('login1', 'login2')
+
+print(f"L'étudiant {binome[0]} est en binôme avec l'étudiant {binome[1]}")
+
+binome = binome + ('login3',)
+print(f"Le trinôme est composé de {binome}")
