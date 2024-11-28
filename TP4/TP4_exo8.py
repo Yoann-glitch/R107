1f14c82b53566aaed0da675f956560d16c1759d3
install <yoyo.mul007@gmail.com>
2024-11-28 09:05:16 +0100

    TP4

---
+++ TP4/TP4_exo8.py
@@ -0,0 +1,9 @@
+info = {
+    'Prénom': 'Yoann',
+    'Nom': 'Muller',
+    'promo': 'RT1',
+    'groupe': 'RT13'
+}
+
+print(f"votre nom est '{info['Nom']}', prénom est '{info['Prénom']}', "
+      f"vous faites partie de la promo '{info['promo']}' et votre groupe est '{info['groupe']}'")
