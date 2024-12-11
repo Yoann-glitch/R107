64aec1d1ec52aaf9dd8c7622bf80f38712436a46
install <yoyo.mul007@gmail.com>
2024-12-11 08:46:28 +0100

    TP5

--- TP5/TP5_exo7_Verification.py
+++ TP5/TP5_exo7_Verification.py
@@ -2,11 +2,9 @@ import os.path
 import datetime
 
 def main():
-    # Demande à l'utilisateur d'entrer les noms des fichiers
     fichier1 = input("Entrez le nom du premier fichier : ")
     fichier2 = input("Entrez le nom du deuxième fichier : ")
 
-    # Vérifie si les fichiers existent
     if os.path.isfile(fichier1):
         taille1 = os.path.getsize(fichier1)
         print(f"Le fichier '{fichier1}' existe et sa taille est de {taille1} octets.")
@@ -23,7 +21,6 @@ def main():
         print(f"Le fichier '{fichier2}' n'existe pas.")
         return
 
-    # Compare les dates de modification
     if date_modif1 > date_modif2:
         date_modif1_formatee = datetime.datetime.fromtimestamp(date_modif1)
         print(f"Le fichier '{fichier1}' est le plus récent, modifié le {date_modif1_formatee}.")
